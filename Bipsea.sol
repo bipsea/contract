3ec372d25eaad457ad005ea523eb35db9a3c4395
Leon Do <leondo123@gmail.com>
2022-08-22 21:33:05 -0400

    Update Bipsea.sol

--- Bipsea.sol
+++ Bipsea.sol
@@ -11,6 +11,7 @@ contract Bipsea {
         address investor;   // Address of user paying for tx fees. Gets 1% of sale
         string  uri;        // Metadata uri: https://ipfs.io/ipfs/bafkreigjdoplg6qattgtkx7zrfreky3xjk52dpxeqxf7bqx7funa2z6vpu
         uint256 price;      // Price of item in smallest unit (wei)
+        bool    canBuy;     // Boolean to enable buying
     }
 
     // Stores all items: itemId => Item
@@ -19,18 +20,14 @@ contract Bipsea {
     // Check if itemId has been purchaed by buyer: itemId => buyer address => true
     mapping(uint256 => mapping(address => bool)) public purchase;
 
-    // Balance of sellers: 0xAlice => 99 wei
-    mapping(address => uint256) public balances;
-
     // Events
     event Sell(uint256 _itemId);
     event Buy(uint256 indexed _itemId, address indexed _buyer, uint256 _amount);
-    event Withdraw(address indexed _sellerAddress, uint256 _sellerAmount, address _withdrawerAddress, uint256 _withdrawerAmount);
-    event Delist(uint64 indexed _itemId, address indexed _seller);
+    event Delist(uint256 indexed _itemId, address indexed _seller);
 
     // Constructor
     constructor() {
-        owner == msg.sender;
+        owner = msg.sender;
     }
 
     // Sell item
@@ -43,14 +40,15 @@ contract Bipsea {
     ) public {
         require(items[_itemId].seller == address(0), "Item Already Listed");
         require(items[_itemId].investor == address(0), "Item Already Listed");
-        // create item to sell
+        // Create item to sell
         items[_itemId] = Item({
             seller: _seller,
             investor: _investor,
             uri: _uri,
-            price: _price
+            price: _price,
+            canBuy: true
         });
-        // emit Sell event
+        // Emit Sell event
         emit Sell(_itemId);
     }
 
@@ -62,45 +60,27 @@ contract Bipsea {
         purchase[_itemId][msg.sender] = true;
         // Seller gets 99% of sale
         uint256 sellerValue = (msg.value * 99) / 100;
-        // Investory gets 1% of sale
+        // Investor gets 1% of sale
         uint256 investorValue = (msg.value * 1) / 100;
-        // Deposit into seller's account
-        balances[items[_itemId].seller] += sellerValue;
-        // Depost into investor's account
-        balances[items[_itemId].investor] += investorValue;
-        // emit Buy event
+        // Send seller value to seller's account
+        payable(items[_itemId].seller).transfer(sellerValue);
+        // Send investor value to investor's account
+        payable(items[_itemId].investor).transfer(investorValue);
+        // Emit Buy
         emit Buy(_itemId, msg.sender, msg.value);
-    }
-
-    // Withdraw funds
-    function withdraw(address _sellerAddress) public payable {
-        require(balances[_sellerAddress] / 10000 * 10000 == balances[_sellerAddress], "Balance too small");
-        // get withdrawer address
-        address withdrawerAddress = msg.sender;
-        // withdrawer gets 0.01% or 1 basis point
-        uint256 withdrawerAmount = balances[_sellerAddress] / 10000;
-        // seller gets 99.99%
-        uint256 sellerAmount = balances[_sellerAddress] - (balances[_sellerAddress] / 10000);
-        // set balance to 0
-        balances[_sellerAddress] = 0;
-        // transfer seller amount
-        payable(_sellerAddress).transfer(sellerAmount);
-        // transfer withdrawer amount
-        payable(withdrawerAddress).transfer(withdrawerAmount);
-        // emit Withdraw event
-        emit Withdraw(_sellerAddress, sellerAmount, withdrawerAddress, withdrawerAmount);
     }
 
     // Delist Item
     function delist(uint64 _itemId) public {
         require(msg.sender == items[_itemId].seller || msg.sender == owner, "Only seller || owner can delist");
-        delete items[_itemId];
+        items[_itemId].canBuy = false;
+        // Emit Delist
         emit Delist(_itemId, msg.sender);
     }
 
     // Set Owner
     function setOwner(address _newOwner) public {
-        require(msg.sender == owner, "Only owner set");
+        require(msg.sender == owner, "Only owner can set");
         owner = _newOwner;
     }
 
