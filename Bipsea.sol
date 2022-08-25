1c321d596a6def6c85cbb28f0d4fd7e44e1a2391
Leon Do <leondo123@gmail.com>
2022-08-24 23:09:18 -0400

    Update Bipsea.sol

--- Bipsea.sol
+++ Bipsea.sol
@@ -24,7 +24,7 @@ contract Bipsea {
     event Sell(uint256 _itemId);
     event Buy(uint256 indexed _itemId, address indexed _buyer, uint256 _amount);
     event Delist(uint256 indexed _itemId, address indexed _seller);
-    event Revenue(address indexed _address, uint256 _amount);
+    event Revenue(uint256 indexed _itemId, address indexed _address, uint256 _amount);
 
     // Constructor
     constructor() {
@@ -70,8 +70,8 @@ contract Bipsea {
         // Emit Buy
         emit Buy(_itemId, msg.sender, msg.value);
         // Emit Revenue
-        emit Revenue(items[_itemId].seller, sellerValue);
-        emit Revenue(items[_itemId].investor, investorValue);
+        emit Revenue(_itemId, items[_itemId].seller, sellerValue);
+        emit Revenue(_itemId, items[_itemId].investor, investorValue);
     }
 
     // Delist Item
